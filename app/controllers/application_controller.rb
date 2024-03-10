06a8215c6ac833f2876021bfeb60a2b55977a221
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2024-03-10 18:09:08 +0100

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_devise_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :first_name, :last_name, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :first_name, :last_name, :username])
+  end
 end
