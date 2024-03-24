9c5b9f77d6b4242a507f4a42916b43f133db6752
Oleksandra <alexandraborovik03@gmail.com>
2024-03-24 17:57:34 +0200

    Update controllers/products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:subcategory_id, :name, :price, :height, :width, :length, :color, :material, :producer, :availability)
+      params.require(:product).permit(:subcategory_id, :name, :price, :height, :width, :length, :color, :material, :producer_id, :availability)
     end
 end
