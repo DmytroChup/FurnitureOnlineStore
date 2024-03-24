a04bac673bc9390cff8957c00bea5e2cab14d00f
Oleksandra <alexandraborovik03@gmail.com>
2024-03-24 17:59:14 +0200

    Add new design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_action :show_footer
+
+  private
+  def show_footer
+    if request.path.include?('/users/sign_in') ||
+      request.path.include?('/users/sign_up') ||
+      request.path.include?('/users/password')
+      @show_footer = false
+    else
+      @show_footer = true
+    end
+  end
 
   protected
   def configure_devise_permitted_parameters
