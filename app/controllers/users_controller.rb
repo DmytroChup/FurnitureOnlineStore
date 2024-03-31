3880206cc31c076956454783d7b852c3c580b844
Oleksandra <alexandraborovik03@gmail.com>
2024-04-01 00:49:56 +0300

    Update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,8 +36,31 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
+    @user = User.find(params[:id])
+
+    # Проверяем, если текущий пароль введен правильно
+    if params[:user][:password].present? && !@user.valid_password?(params[:user][:current_password])
+      @user.errors.add(:current_password, "is invalid")
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+      return
+    end
+
+    # Проверяем, что новый пароль совпадает с подтверждением
+    if params[:user][:password].present? && params[:user][:password] != params[:user][:password_confirmation]
+      @user.errors.add(:password_confirmation, "doesn't match New password")
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+      return
+    end
+
     respond_to do |format|
       if @user.update(user_params)
+        bypass_sign_in(@user) # автоматический вход после обновления пароля
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -46,6 +69,19 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  # PATCH/PUT /users/1 or /users/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @user }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /users/1 or /users/1.json
   def destroy
@@ -65,6 +101,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:user_role, :first_name, :patronymic, :last_name, :phone, :username, :password, :cart_id)
+      params.require(:user).permit(:user_role, :first_name, :patronymic, :last_name, :phone, :username, :password, :password_confirmation, :cart_id)
+      # params.require(:user).permit(:user_role, :first_name, :patronymic, :last_name, :phone, :username, :password, :cart_id)
     end
 end
