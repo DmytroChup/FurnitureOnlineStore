7ca7c5fa57950fd1cd88b15edde140b8c2e1733f
Oleksandra <alexandraborovik03@gmail.com>
2024-04-21 11:18:45 +0300

    Add translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
   before_action :show_footer
 
+  around_action :switch_locale
+
   private
   def show_footer
     if request.path.include?('/users/sign_in') ||
@@ -16,6 +18,21 @@ class ApplicationController < ActionController::Base
     else
       @show_footer = true
     end
+  end
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 
   protected
