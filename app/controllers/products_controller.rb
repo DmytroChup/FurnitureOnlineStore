13f8d458fb7d2634d0b29e16e425a89fefa9d9b3
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2024-05-13 20:41:19 +0200

    Add preferred currency to index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,20 +6,13 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    @usd_buy, @eur_buy = find_currency
     # @products = Product.all.order(:name)
   end
 
   # GET /products/1 or /products/1.json
   def show
-    @currency_rates = PrivatbankApi.fetch_currency_rates
-    @currency_rates.each do |rate|
-      case rate["ccy"]
-      when "USD"
-        @usd_buy = rate["buy"].to_f
-      when "EUR"
-        @eur_buy = rate["buy"].to_f
-      end
-    end
+    @usd_buy, @eur_buy = find_currency
   end
 
   # GET /products/new
@@ -90,5 +83,18 @@ class ProductsController < ApplicationController
     params[:product][:price] = params[:product][:price].to_s
     params.require(:product).permit(:subcategory_id, :name, :price, :height, :width, :length, :color, :material,
                                     :producer_id, :availability)
+  end
+
+  def find_currency
+    @currency_rates = PrivatbankApi.fetch_currency_rates
+    @currency_rates.each do |rate|
+      case rate["ccy"]
+      when "USD"
+        @usd_buy = rate["buy"].to_f
+      when "EUR"
+        @eur_buy = rate["buy"].to_f
+      end
+    end
+    [@usd_buy, @eur_buy]
   end
 end
