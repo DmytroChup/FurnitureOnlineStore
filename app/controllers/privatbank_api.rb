5982548cbbd255e48c4ce821fc9acf8902e09646
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2024-05-13 13:23:34 +0200

    Fix rubocop

--- app/controllers/privatbank_api.rb
+++ app/controllers/privatbank_api.rb
@@ -1,9 +1,11 @@
-require 'net/http'
-require 'uri'
-require 'json'
+# frozen_string_literal: true
+
+require "net/http"
+require "uri"
+require "json"
 
 class PrivatbankApi
-  API_URL = URI('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+  API_URL = URI("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
 
   def self.fetch_currency_rates
     response = Net::HTTP.get(API_URL)
--- db/migrate/20240512101412_create_users_subcategories.rb
+++ db/migrate/20240512101412_create_users_subcategories.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUsersSubcategories < ActiveRecord::Migration[7.1]
   def change
     create_table :users_subcategories do |t|
